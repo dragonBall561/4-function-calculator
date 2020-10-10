e2c25b77f3d2cee417012c043093f6d37b5c8e13
dragonBall561 <61358474+dragonBall561@users.noreply.github.com>
2020-10-10 12:18:20 -0400

    Added Everything

---
+++ Calculator/Calc.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo '     ____               ____                    ________  ____       ____'
+echo '    /     /|     /     /      /    /  /     /|     /     /    |     /   |'
+echo '   /     / |    /     /      /    /  /     / |    /     /     |    /    /' 
+echo '  /     /__|   /     /      /    /  /     /__|   /     |      /   /____/'
+echo ' /     /   |  /     /      /    /  /     /   |  /      |     /   /     |'
+echo '/____ /    | /____ /____  /____/  /____ /    | /       |____/   /      | '
+
+
+split_ex(){
+	export ex1=$1
+	export ex2=$2
+	export ex3=$3
+}
+
+
+echo This is a Four-Function Calculator
+echo Insert Your Number, Followed by a '/', '*', '+', or '-', then another number
+echo For example, '3 - 4' or '3 * 4'
+echo Please use spaces in between the number\(s\) and the symbols
+read -p 'Please Write Your Equation Here: ' to_calc
+echo $to_calc
+split_ex $to_calc
+
+if [ $ex2 = 'Calc.sh' ]; then
+	expr $1 \* $3
+else
+	echo 2
+	expr $ex1 $ex2 $ex3
+	exit 0
+fi
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# 4-function-calculator
- 
+# A Four Function Calculator
+This is a Four Function Calculator Written in Shell Script.
+NOTE: **_PLEASE_** keep Calc.sh in its own directory. Because of the way that I have written this, it needs to be in its own directory with no other files or folders
